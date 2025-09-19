4b524dc7b1dda039a5620c80a203817c1a37fe4f
xcixor <pndungu54@gmail.com>
2025-09-20 02:56:20 +0300

    fix(cors): update CORS_ALLOWED_ORIGINS to specify allowed origins

--- app/base.py
+++ app/base.py
@@ -26,7 +26,11 @@ ALLOWED_HOSTS = ["*"]
 
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "https://eld-route-planner-staging-1018057487898.us-east1.run.app"
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
