1623a6690889b4515d28c056d08f839543d3dff8
xcixor <pndungu54@gmail.com>
2025-09-20 02:51:30 +0300

    fix(cors): simplify CORS_ALLOWED_ORIGINS to allow all origins

--- app/base.py
+++ app/base.py
@@ -26,10 +26,7 @@ ALLOWED_HOSTS = ["*"]
 
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
