a2ed4cf52d471d48e661e4457d606db434030bf4
xcixor <pndungu54@gmail.com>
2025-09-29 13:26:37 +0300

    feat(settings): add frontend staging URL to CORS and CSRF trusted origins

--- app/base.py
+++ app/base.py
@@ -29,7 +29,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://eld-route-planner-staging-1018057487898.us-east1.run.app"
+    "https://eld-route-planner-staging-1018057487898.us-east1.run.app",
+    "https://eld-frontend-staging-1018057487898.us-east1.run.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
@@ -37,7 +38,8 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://eld-route-planner-staging-1018057487898.us-east1.run.app"
+    "https://eld-route-planner-staging-1018057487898.us-east1.run.app",
+    "https://eld-frontend-staging-1018057487898.us-east1.run.app"
 ]
 
 # Application definition
